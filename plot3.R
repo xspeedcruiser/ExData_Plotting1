b02f813501cb9c499ddb1e2dd300dabb78c6dc5d
skat <xspeedcruiser@gmail.com>
2014-05-12 01:34:06 +0800

    added plot3

---
+++ plot3.R
@@ -0,0 +1,11 @@
+source("loaddata.R")
+
+data <- loaddata()
+
+png(filename = "plot3.png", width = 480, height = 480, units = "px")
+cols = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
+plot(data$DateTime, data$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
+lines(data$DateTime, data$Sub_metering_2, type="l", col="red")
+lines(data$DateTime, data$Sub_metering_3, type="l", col="blue")
+legend("topright", lty=1, lwd=1, col=c("black","blue","red"), legend=cols)
+dev.off()
